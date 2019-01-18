e492c9b9807a9367cd123e94efbeebebc1817bca
Lievin Christopher <lievin.christopher@gmail.com>
2019-01-18 17:58:19 +0100

    refix install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
+if [[ -d MEGAcloud && -d /usr/local/src/megacloud/sdk && -f /usr/local/src/megacloud/sdk/Makefile ]]
+then
+    cd /usr/local/src/megacloud/sdk
+    make examples/megasimplesync CPPFLAGS=-Wno-deprecated-declarations
+    make examples/linux/megafuse 
+    sudo ln -s examples/megasimplesync /usr/bin/
+    sudo ln -s examples/linux/megafuse /usr/bin/
+    exit 0
+fi
 git clone https://github.com/lievin-christopher/MEGAcloud
 cd MEGAcloud
-sudo mkdir /usr/src/megacloud
-sudo cp *.py LICENSE /usr/src/megacloud/
-sudo ln -s /usr/src/megacloud/main.py /usr/bin/megacloud
+echo "install MEGAcloud"
+sudo mkdir /usr/local/src/megacloud
+sudo chmod 775 /usr/local/src/megacloud
+sudo cp *.py LICENSE /usr/local/src/megacloud
+sudo chown -R root:users /usr/local/src/megacloud
+sudo ln -s /usr/local/src/megacloud/main.py /usr/local/bin/megacloud
+echo "install MEGAcloud config"
 mkdir ~/.config/megacloud
 cp sync.conf.json ~/.config/megacloud/
+cd /usr/local/src/megacloud
 git clone https://github.com/meganz/sdk
-cp megafuse/megafuse.cpp sdk/examples/linux/
-cd sdk
-sh autogen.sh
-./configure --with-fuse --without-freeimage --without-sodium --without-sqlite
+cd -
+cp megafuse/megafuse.cpp /usr/local/src/megacloud/sdk/examples/linux/
+cd /usr/local/src/megacloud/sdk
+echo "configure MegaSDK"
+sh autogen.sh && ./configure --with-fuse --without-freeimage --without-sodium --without-sqlite
+if [[ ! -f Makefile ]]
+then
+    
+    sh autogen.sh && ./configure --with-fuse --without-freeimage --without-sodium --without-sqlite
+    if [[ ! -f Makefile ]]
+    then
+        echo """
+        Fail to configure MegaSDK
+        Please fix there issues and run me again:
+        cd /usr/local/src/megacloud/sdk
+        sh autogen.sh
+        ./configure --with-fuse --without-freeimage --without-sodium --without-sqlite
+        """
+        exit 1
+    fi
+fi
 make examples/megasimplesync CPPFLAGS=-Wno-deprecated-declarations
 make examples/linux/megafuse 
-sudo cp examples/megasimplesync /usr/bin/
-sudo cp examples/linux/megafuse /usr/bin/
-cd .. && rm -rf sdk
-cd .. && rm -rf MEGAcloud
+sudo ln -s /usr/local/src/megacloud/sdk/examples/megasimplesync /usr/local/bin/
+sudo ln -s /usr/local/src/megacloud/sdk/examples/linux/megafuse /usr/local/bin/
+cd - && cd .. && rm -rf MEGAcloud
